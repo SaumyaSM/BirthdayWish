cb7c6c6a1ed836ac4c9ba1c20c06eae148d11731
SaumyaSM <saumyarsm@gmail.com>
2025-06-19 18:11:55 +0530

    updates

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
-    $(".container").mouseenter(function() {
-        $(".card").stop().animate({
-            top: "-90px"
-    },"slow");
-    }).mouseleave(function(){
-        $(".card").stop().animate({
-            top: "0px"
-    },"slow");
+    let envelopeOpen = false;
+    
+    // Handle envelope click
+    $(".envelope-wrapper").click(function() {
+        $(this).toggleClass('flap');
+        envelopeOpen = !envelopeOpen;
+        
+        // If envelope is opened, trigger the card hover effect
+        if (envelopeOpen) {
+            setTimeout(function() {
+            $(".card").stop().animate({
+                top: "-90px"
+            }, "200");
+        }, 900); 
+          $(".hearts").addClass("no-animation");// Delay to allow flap animation to complete
+        } else {
+            // If envelope is closed, return card to original position
+            $(".card").stop().animate({
+                top: "0px"
+            }, "slow");
+             setTimeout(function () {
+                $(".hearts").removeClass("no-animation");
+            }, 900); // matches envelope flap transition timing
+        }
     });
 });
